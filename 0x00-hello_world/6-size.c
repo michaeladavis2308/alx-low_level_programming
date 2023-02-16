dd207f2cdb18e7b203f7ad1abc087afae20de71e
michaeladavis2308 <michaeladavis2308@gmail.com>
2023-02-16 11:16:38 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return :Always (0) (Success)
+ *
+ */
+
+int main(void)
+{
+	int integerType;
+	float floatType;
+       double doubleType;
+	char charType;
+
+	printf("Sixe of int: %ld bytes\n",sizeof(integerType));
+	printf("Size of float %ld bytes\n",sizeof(floatType));
+	printf("Size of double %ld bytes\n",sizeof(doubleType));
+	printf("Size of char %ld bytes\n",sizeof(charType));
+	
+	return (0);	
+}
