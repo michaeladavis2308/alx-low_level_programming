ad9189c55e91f37ea40982480481d60a6df888b1
michaeladavis2308 <michaeladavis2308@gmail.com>
2023-02-17 21:07:05 -0800

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main-uses putchar and for loop to print alphabets in reverse
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
