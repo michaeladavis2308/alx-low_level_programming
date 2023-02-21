3112a1a209b3eaf548b447d0ed96f4846d886038
michaeladavis2308 <michaeladavis2308@gmail.com>
2023-02-21 07:04:00 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description - a program that prints _putchar, followed by a new line.
+ *
+ * Return:Always 0 success
+ *
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
