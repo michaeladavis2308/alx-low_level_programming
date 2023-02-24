160e563c324e030e3ee523fab2048ba76f2a6fda
michaeladavis2308 <michaeladavis2308@gmail.com>
2023-02-23 17:38:12 -0800

    more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers-  a fxn that prints 10x the numbers, frm 0 - 14
+ * Return :void
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
+
+
