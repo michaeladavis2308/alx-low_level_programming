7be3070c79c00f552fbecb59fb8920b81f737f14
michaeladavis2308 <michaeladavis2308@gmail.com>
2023-03-02 02:00:00 -0800

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcmp - a fxn that compares two strings.
+ * @s1:the first string to be compared
+ * @s2:the second string to be compared
+ * Return:int(>0 if greater than, <0 if less than and 0 if equal)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
