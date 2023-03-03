1f622e70c50bda370f8be6251a57e2859b752c06
michaeladavis2308 <michaeladavis2308@gmail.com>
2023-03-02 16:10:12 -0800

    cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * cap_string - capitalises all wrds & uses special x'ters as separators
+ * @n:the elements of the string
+ * Return:an upperchar
+ */
+char *cap_string(char *n)
+{
+	int i, x;
+	int cap = 32;
+	int separators[] = {',', ';', '.', '?', '"',
+		'(', ')', '{', '}', ' ', '\n', '\t'};
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+		{
+			n[i] = n[i] - cap;
+		}
+		cap = 0;
+
+		for (x = 0; x <= 12; x++)
+		{
+			if (n[i] == separators[x])
+			{
+				x = 12;
+				cap = 32;
+			}
+		}
+	}
+	return (n);
+}
